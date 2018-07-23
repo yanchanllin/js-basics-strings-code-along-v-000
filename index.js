ace8beec2ba2de0ef10aedc123ae9625c4df1c82
YanC Lin <yonalin1@gmail.com>
2018-07-23 18:36:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
-const welcomeMessage = 'Welcome to Flatbook, + ${currentUser}!`;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
